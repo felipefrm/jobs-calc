f4ed59c325405e75b71773d1287afc62cb715199
Felipe Melo <narulee@hotmail.com>
2022-02-10 15:58:44 -0300

    database

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -9,12 +9,8 @@ module.exports = {
     return res.render("job")
   },
 
-  save(req, res) {
-    const jobs = Job.get()
-    const lastId = jobs[jobs.length - 1]?.id || 0;
-
-    jobs.push({
-      id: lastId + 1,
+  async save(req, res) {
+    await Job.create({
       name: req.body.name,
       "daily-hours": req.body["daily-hours"], 
       "total-hours": req.body["total-hours"], 
@@ -24,11 +20,11 @@ module.exports = {
     return res.redirect('/')
   },
 
-  show(req, res) {
+  async show(req, res) {
     const jobId = req.params.id
 
-    const jobs = Job.get()
-    const profile = Profile.get()
+    const jobs = await Job.get()
+    const profile = await Profile.get()
 
     const job = jobs.find(job => Number(job.id) === Number(jobId))
 
@@ -41,41 +37,24 @@ module.exports = {
     return res.render("job-edit", { job })
   },
 
-  update(req, res) {
+  async update(req, res) {
     const jobId = req.params.id
 
-    const jobs = Job.get()
-
-    const job = jobs.find(job => Number(job.id) === Number(jobId))
-
-    if (!job) {
-      return res.send('Job not found!')
-    }
-
     const updatedJob = {
-      ...job, 
       name: req.body.name,
       "total-hours": req.body["total-hours"], 
       "daily-hours": req.body["daily-hours"], 
     }
 
-    const newJob = jobs.map(job => {
-      if(Number(job.id) === Number(jobId)) {
-        job = updatedJob
-      }
-      
-      return job
-    })
+    await Job.update(updatedJob, jobId)
 
-    Job.update(newJob)
-
-    res.redirect('/job/' + jobId)
+    res.redirect('/')
   },
 
-  delete(req, res) {
+  async delete(req, res) {
     const jobId = req.params.id
 
-    Job.delete(jobId)
+    await Job.delete(jobId)
 
     return res.redirect('/')
   }
--- src/models/Job.js
+++ src/models/Job.js
@@ -1,28 +1,55 @@
-let data = [
-  {
-    id: 1,
-    name: "Pizzaria Guloso",
-    "daily-hours": 2,
-    "total-hours": 1,
-    created_at: Date.now(),
-  },
-  {
-    id: 2,
-    name: "OneTwo Project",
-    "daily-hours": 3,
-    "total-hours": 47,
-    created_at: Date.now(),
-  },
-];
+const Database = require("../db/config");
 
 module.exports = {
-  get() {
-    return data;
+  async get() {
+    const db = await Database();
+
+    const data = await db.all(`SELECT * FROM jobs`);
+
+    await db.close();
+
+    return data.map((job) => ({
+      id: job.id,
+      name: job.name,
+      "daily-hours": job.daily_hours,
+      "total-hours": job.total_hours,
+      created_at: job.created_at,
+    }));
   },
-  update(newData) {
-    data = newData;
+  async update(data, id) {
+    const db = await Database();
+
+    await db.run(`UPDATE jobs SET
+    name = "${data.name}",
+    daily_hours = ${data['daily-hours']},
+    total_hours = ${data['total-hours']}
+    WHERE id = ${id}
+    `)
+
+    await db.close();
   },
-  delete(id) {
-    data = data.filter(job => Number(job.id) !== Number(id))
-  }
+  async delete(id) {
+    const db = await Database();
+
+    await db.run(`DELETE FROM jobs WHERE id = ${id}`);
+
+    await db.close();
+  },
+  async create(newData) {
+    const db = await Database();
+
+    await db.run(`INSERT INTO jobs (
+      name,
+      daily_hours,
+      total_hours,
+      created_at
+    ) VALUES (
+      "${newData.name}",
+      ${newData['daily-hours']},
+      ${newData['total-hours']},
+      ${newData['created_at']}
+    )`);
+
+    await db.close();
+  },
 };
